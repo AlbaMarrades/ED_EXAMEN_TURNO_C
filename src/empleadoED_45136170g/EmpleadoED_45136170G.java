08a599f60237379f557c73977eb35cf914d94e74
Alba Marrades <marrades.alba@gmail.com>
2021-01-14 10:24:15 +0100

    Añadido el constructor

--- src/empleadoED_45136170g/EmpleadoED_45136170G.java
+++ src/empleadoED_45136170g/EmpleadoED_45136170G.java
@@ -8,8 +8,16 @@ public class EmpleadoED_45136170G {
     private String nombreCompleto, dni;
     private double salarioBasePorHora = 10.56;
 
+    public EmpleadoED_45136170G(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
     public static void main(String[] args) {
-       
+        EmpleadoED_45136170G emp = new EmpleadoED_45136170G("ALBA MARRADES", "45136170G");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
     }
 
 }
