a697a2f452918e2bdcd29fb7ab9d6104de3df6ac
Alba Marrades <marrades.alba@gmail.com>
2021-01-14 10:20:50 +0100

    Creado proyecto empleadoED con variables

---
+++ src/empleadoED_45136170g/EmpleadoED_45136170G.java
@@ -0,0 +1,15 @@
+package empleadoED_45136170g;
+
+import java.util.*;
+
+public class EmpleadoED_45136170G {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public static void main(String[] args) {
+       
+    }
+
+}
